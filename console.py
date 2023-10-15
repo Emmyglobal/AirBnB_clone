bba7ee56228834e83a012f58fd8b549385d08a7d
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-10-15 12:46:11 -0700

    Task 7 and 8

--- console.py
+++ console.py
@@ -3,10 +3,14 @@
 A program that contains the entry point of the command interpreter
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
+    __classes = ["BaseModel", "User"]
 
     def do_quit(self, arg):
         """Quit command to exit the program
@@ -20,6 +24,89 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         return
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            """Prints if class is missing
+            """
+            print("** class name missinng **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+
+        else:
+            new_object = eval(f"{args[0]}")()
+            print(new_object.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based ont the class name
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print("** no instance found **")
+        else:
+            print(storage.all()[f"{args[0]}.{args[1]}"])
+    
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print("** no instance found **")
+        else:
+            del storage.all()[f"{args[0]}.{args[1]}"]
+            storage.save()
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print([str(value) for val in storage.all().values()])
+        elif args[0] not in sel.__classes:
+            print("** class doesn't exist **")
+        else:
+            print([str(value) for key, value in storage.all().items() if key.startswith(args[0])])
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute
+        """
+
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print("** no instance found **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Defines a User Class"""
+""" Defines a User Class that inherits from BaseModel"""
 
 from models.base_model import BaseModel
 
